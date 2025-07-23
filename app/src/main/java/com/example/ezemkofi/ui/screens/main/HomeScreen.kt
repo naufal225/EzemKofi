1212f59bb998387ef12e855cc097f6739341f45b
naufal225 <naufalmarufashrori225@gmail.com>
2025-07-23 13:19:33 +0700

    menambahkan badge pada cart

--- app/src/main/java/com/example/ezemkofi/ui/screens/main/HomeScreen.kt
+++ app/src/main/java/com/example/ezemkofi/ui/screens/main/HomeScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Badge
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
@@ -41,6 +42,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -142,12 +144,30 @@ fun HomeScreen(navController: NavController, coffeeViewModel: CoffeeViewModel) {
                         },
                         colors = IconButtonDefaults.iconButtonColors(EzemWhite)
                     ) {
-                        Icon(
-                            painter = painterResource(R.drawable.shopping_bag),
-                            contentDescription = "shoppingBag",
-                            Modifier.size(28.dp),
+                        Box(
+                            modifier = Modifier.size(32.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.shopping_bag),
+                                contentDescription = "shoppingBag",
+                                Modifier.size(28.dp),
 
-                        )
+                                )
+                            if(sharedPrefsManager.getCartItems().isNotEmpty()) {
+                                Badge(
+                                    containerColor = Color.Red,
+                                    modifier = Modifier.clip(CircleShape).align(Alignment.TopEnd)
+                                ) {
+                                    Text(
+                                        text = sharedPrefsManager.getCartItems().count().toString(),
+                                        color = Color.White,
+                                        fontSize = 12.sp,
+                                        fontFamily = Poppins
+                                    )
+                                }
+                            }
+                        }
                     }
                 }
 
